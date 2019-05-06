dc8e8ef794ff62961f79ff0b36934f4c59675446
peperoschach <luiz.felipe_23@yahoo.com.br>
2019-05-06 15:13:54 -0300

    Create PageAdmin

---
+++ vendor/peperoschach/php-classes/src/PageAdmin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Peperoschach;
+
+use Rain\Tpl;
+
+class PageAdmin extends Page {
+
+    public function __construct($opts = array(), $tpl_dir = "/ecommerce/views/admin/")
+    {
+        parent::__construct($opts, $tpl_dir);
+    }
+}
