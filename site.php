cc21254b2794d992dbce39e32b7c3049b028070d
peperoschach <luiz.felipe_23@yahoo.com.br>
2019-05-13 11:02:45 -0300

    Create pagination

--- site.php
+++ site.php
@@ -17,14 +17,28 @@ $app->get('/', function () {
 
 $app->get("/categories/:idcategory", function ($idcategory) {
 
+    $pageCurrent = (isset($_GET['page'])) ? (int)$_GET["page"] : 1;
+
     $category = new Category();
 
     $category->get((int)$idcategory);
+
+    $pagination = $category->getProductsPage($pageCurrent);
+
+    $pages = [];
+
+    for ($i=1; $i <= $pagination['pages']; $i++) {
+        array_push($pages, [
+            'link'=>'/ecommerce/categories/' . $category->getidcategory() . '?page=' . $i,
+            'page'=>$i
+        ]);
+    }
 
     $page = new Page();
 
     $page->setTpl("category", [
         "category" =>$category->getValues(),
-        "products" =>Product::checkList($category->getProducts())
+        "products" =>$pagination["data"],
+        "pages"=>$pages
     ]);
 });
